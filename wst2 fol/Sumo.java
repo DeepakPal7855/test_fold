164733a303e85b7465079d72cc80371bc12260d9
ram sharma <sharmaram2326@gmail.com>
2025-01-25 13:38:39 +0530

    add files

---
+++ wst2 fol/Sumo.java
@@ -0,0 +1,16 @@
+class Sumo
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("taking input from the user");
+int a= sc.nextInt();
+System.out.println("enter num1" );
+Scanner sc = new Scanner(System.in);
+System.out.println("taking input from the user");
+int b= sc.nextInt();
+System.out.println("enter num2" ); 
+int sum =a+b;
+System.out.println(sum);
+}
+}
